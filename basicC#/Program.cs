3d991717b4b6f7e9f16f4fad3e24c97a554b34e1
Quixar <145463262+Quixar@users.noreply.github.com>
2024-06-13 21:52:53 +0300

    Add files via upload

---
+++ basicC#/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace basicC_
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 40;
+            Console.CursorVisible = false;
+            int x = 5;
+            int y = 5;
+            while (true)
+            {
+                ConsoleKeyInfo k = Console.ReadKey(true);
+
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.Write(" ");
+                if (k.Key == ConsoleKey.LeftArrow && x > 0)
+                {
+                    x--;
+                }
+                else if (k.Key == ConsoleKey.RightArrow && x <= Console.WindowWidth)
+                {
+                    x++;
+                }
+                else if (k.Key == ConsoleKey.UpArrow && y > 0)
+                {
+                    y--;
+                }
+                else if (k.Key == ConsoleKey.DownArrow && y <= Console.WindowHeight)
+                {
+                    y++;
+                }
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.WriteLine((char)1);
+            }
+            Thread.Sleep(15);
+        }
+    }
+}
+ 
